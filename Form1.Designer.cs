96301b70f5be2bd633acc959b4fcdf5522621df0
jsamu <jsamu@DESKTOP-5EHEBUS>
2025-06-20 18:03:11 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace EjercicioSonidoYCargarImagen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSonido = new System.Windows.Forms.Button();
+            this.txtDirSonido = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnAsignar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSonido
+            // 
+            this.btnSonido.Location = new System.Drawing.Point(12, 51);
+            this.btnSonido.Name = "btnSonido";
+            this.btnSonido.Size = new System.Drawing.Size(75, 23);
+            this.btnSonido.TabIndex = 0;
+            this.btnSonido.Text = "Escuchar";
+            this.btnSonido.UseVisualStyleBackColor = true;
+            this.btnSonido.Click += new System.EventHandler(this.btnSonido_Click);
+            // 
+            // txtDirSonido
+            // 
+            this.txtDirSonido.Location = new System.Drawing.Point(12, 25);
+            this.txtDirSonido.Name = "txtDirSonido";
+            this.txtDirSonido.Size = new System.Drawing.Size(263, 20);
+            this.txtDirSonido.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(28, 135);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(275, 150);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnAsignar
+            // 
+            this.btnAsignar.Location = new System.Drawing.Point(112, 332);
+            this.btnAsignar.Name = "btnAsignar";
+            this.btnAsignar.Size = new System.Drawing.Size(75, 23);
+            this.btnAsignar.TabIndex = 3;
+            this.btnAsignar.Text = "Asignar";
+            this.btnAsignar.UseVisualStyleBackColor = true;
+            this.btnAsignar.Click += new System.EventHandler(this.btnAsignar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(330, 450);
+            this.Controls.Add(this.btnAsignar);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtDirSonido);
+            this.Controls.Add(this.btnSonido);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSonido;
+        private System.Windows.Forms.TextBox txtDirSonido;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnAsignar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EjercicioSonidoYCargarImagen
+{
+    public partial class Form1 : Form
+
+    {
+        private SoundPlayer player;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSonido_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Assign the selected file's path to 
+                // the SoundPlayer object.  
+                player.SoundLocation = this.txtDirSonido.Text;
+
+                // Load the .wav file.
+                player.LoadAsync();
+                player.Play();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnAsignar_Click(object sender, EventArgs e)
+        {
+            // Ensure WaitOnLoad is false.
+            pictureBox1.WaitOnLoad = false;
+
+            // Load the image asynchronously.
+            pictureBox1.LoadAsync(@"C:\Users\jsamu\source\repos\EjercicioSonidoYCargarImagen\64663e9017f78.jpeg");
+        }
+    }
+}
